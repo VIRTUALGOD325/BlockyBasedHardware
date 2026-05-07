b987471504863faf94a5203a69599a67b7edb966
TANISHQ AMIT NABAR <65289865+VIRTUALGOD325@users.noreply.github.com>
2026-05-07 18:07:58 +0530

    Fix base path for server deployment

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/hardware/',
   plugins: [react()],
   server: {
+    host: '0.0.0.0',
     allowedHosts: ["hardware-gui", "localhost"]
   }
 })
